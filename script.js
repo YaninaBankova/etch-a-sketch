40b6a862aa9fb95697c28bddf803e9c475bfacc6
YaninaBankova <43926387+YaninaBankova@users.noreply.github.com>
2026-05-05 15:17:43 +0300

    Add button to change number of squares

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-function createGrid(rows, cols){
+function createGrid(rows){
     const container = document.querySelector(".container");
     for(let i = 0; i < rows; i++){
-        for(let j = 0; j < cols; j++){
+        for(let j = 0; j < rows; j++){
             let gridSquare = document.createElement("div");
             gridSquare.classList.add("grid-square");
             container.appendChild(gridSquare);
             gridSquare.addEventListener("mouseenter", hoverEffect);
         }
     }
+
+    const styleSheet = document.styleSheets[0];
+    const gridSquareRule = Array.from(styleSheet.cssRules).find(
+        r => r.selectorText === ".grid-square");
+    gridSquareRule.style.width = `calc((90vh - ${rows - 1}px - 10px) / ${rows})`;
+    gridSquareRule.style.maxWidth = `calc((90vw - ${rows - 1}px - 10px) / ${rows})`;
 }
 
-createGrid(16, 16);
+createGrid(16);
+
+function deleteGrid(){
+     const container = document.querySelector(".container");
+     while(container.firstChild){
+        container.removeChild(container.firstChild);
+     }
+}
 
 function hoverEffect(event){
     const gridSquare = event.target;
     gridSquare.style.backgroundColor = "black";
 }
+
+const resize = document.querySelector("#resize");
+resize.addEventListener("click", (event) => {
+    let numRows = Number(prompt("How many squares per side?", 16));
+    if(numRows > 100){
+        numRows = 100;
+    } else if(isNaN(numRows)){
+        numRows = 16;
+    }
+    deleteGrid();
+    createGrid(numRows);
+});
