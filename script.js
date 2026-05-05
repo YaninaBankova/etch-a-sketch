ba7430a324bafee7928f226269e031ca44e74c1f
YaninaBankova <43926387+YaninaBankova@users.noreply.github.com>
2026-05-05 11:03:20 +0300

    Create 16x16 grid of square divs

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function createGrid(n){
-    
+function createGrid(rows, cols){
+    const container = document.querySelector(".container");
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < cols; j++){
+            let gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            container.appendChild(gridSquare);
+        }
+    }
 }
+
+createGrid(16, 16);
